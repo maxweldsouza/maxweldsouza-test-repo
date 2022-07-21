9ad36ebadfeb63154e79fa68650ea5c24addb77c
Maxwel Dsouza <2565045+maxweldsouza@users.noreply.github.com>
2022-07-21 12:23:32 +0530

    chore: Add semantic release

---
+++ release.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "release": {
+    "branches": ["main", { "name": "beta", "prerelease": true }]
+  },
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/npm",
+      {
+        tarballDir: "release",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: "release/*.tgz",
+      },
+    ],
+    "@semantic-release/git",
+  ],
+  preset: "angular",
+};
